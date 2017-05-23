ef3e83d98332506054dd9be4053c0d397d1d9148
chinmay <chinmaydas1988@gmail.com>
2017-05-22 23:13:06 -0700

    added pagination in user search page

--- modules/member/views/default/updateGroomLocation.php
+++ modules/member/views/default/updateGroomLocation.php
@@ -1,10 +1,12 @@
 <?php
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\lib\Core;
 /* @var $this yii\web\View */
 /* @var $model app\modules\member\models\AppUser */
 /* @var $form yii\widgets\ActiveForm */
 $this->title = "Update Groom location";
+$stateUrl = Yii::$app->getUrlManager()->createUrl(['member/default/stateagainstcountry'])
 ?>
 <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
@@ -17,8 +19,8 @@ $this->title = "Update Groom location";
         <div class="mandate-asset-create">
             <div class="modal-body">
                 <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
-                <?= $form->field($model, 'country')->textInput(['maxlength' => true]) ?>
-                <?= $form->field($model, 'city')->textInput(['maxlength' => true]) ?>
+                <?= $form->field($model, 'country')->dropDownList(Core::getCountryAssoc(),['prompt' => 'Select']) ?>
+                <?= $form->field($model, 'city')->dropDownList(['prompt' => 'Select']) ?>
                 <?= $form->field($model, 'state')->textInput(['maxlength' => true]) ?>
                 <div class="form-group">
                     <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
@@ -29,3 +31,23 @@ $this->title = "Update Groom location";
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    function getState(countryID)
+    {
+        $.ajax({
+            method:'GET',
+            dataType: 'json',
+            url:'<?php echo $stateUrl; ?>',
+            data:{countryID:countryID},
+            beforeSend:function(){
+                $("#usermaster-state").html('<option value="">---</options>');
+            },
+            success:function(response) {
+                $.each(response, function(i, value) {
+                    $('#usermaster-state').append($('<option>').text(value).attr('value', i));
+                });
+            }
+        });
+    }
+</script>
